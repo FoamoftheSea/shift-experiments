19c76a4229046ea8cf6d6a303cb4bb3db5ed7fb2
FoamoftheSea <indezera@gmail.com>
2023-09-11 17:00:57 -0700

    Added helper scripts for wandb copy and merge runs

---
+++ scripts/utils/copy_wandb_run.py
@@ -0,0 +1,71 @@
+from argparse import ArgumentParser
+
+import numpy as np
+import wandb
+
+
+def main(args):
+
+    if args.names is not None:
+        if args.runs is None:
+            raise Exception("Renaming copied runs not supported when copying whole project.")
+        assert len(args.names) == len(args.runs), "Number of new names must equal number of run IDs"
+
+    args.dst_entity = args.dst_entity if args.dst_entity is not None else args.src_entity
+    args.dst_project = args.dst_project if args.dst_project is not None else args.src_project
+    same_project = args.src_entity == args.dst_entity and args.src_project == args.dst_project
+    if same_project and args.names is None:
+        name_append = "-copy"
+    else:
+        name_append = ""
+
+    # Set your API key
+    wandb.login()
+    # Initialize the wandb API
+    api = wandb.Api()
+
+    # Get the runs from the source project
+    runs = api.runs(f"{args.src_entity}/{args.src_project}")
+
+    # Iterate through the runs and copy them to the destination project
+    for run in runs:
+        if args.runs is not None and run.id not in args.runs:
+            continue
+        # Get the run history and files
+        history = run.history(samples=run.lastHistoryStep + 1)
+        files = run.files()
+
+        name = run.name if args.names is None else args.names[args.runs.index(run.id)]
+
+        # Create a new run in the destination project
+
+        # Log the history to the new run
+        new_run = wandb.init(
+            project=args.dst_project,
+            entity=args.dst_entity,
+            config=run.config,
+            name=name + name_append,
+            resume="allow"
+        )
+        for index, row in history.iterrows():
+            new_run.log({k: v for k, v in row.to_dict().items() if not np.isnan(v)})
+
+        # Upload the files to the new run
+        for file in files:
+            file.download(replace=True)
+            new_run.save(file.name, policy="now")
+
+        # Finish the new run
+        new_run.finish()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Copies one or all of the runs in a wandb project to another.")
+    parser.add_argument("-se", "--src-entity", type=str, default="indezera", help="Source wandb entity name.")
+    parser.add_argument("-sp", "--src-project", type=str, help="Name of the wandb projecet.")
+    parser.add_argument("-de", "--dst-entity", type=str, default=None, help="Destination wandb entity name.")
+    parser.add_argument("-dp", "--dst-project", type=str, default=None, help="Name of destination wandb project.")
+    parser.add_argument("-r", "--runs", nargs="*", type=str, default=None, help="List of run IDs to copy. If None will copy all in project.")
+    parser.add_argument("-n", "--names", nargs="*", type=str, default=None, help="List of new names for copied runs (optional).")
+
+    main(parser.parse_args())
