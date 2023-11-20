18d6b471d89b49365cd2912560421c9682373f78
FoamoftheSea <indezera@gmail.com>
2023-11-19 20:15:07 -0800

    Made torch installation extra in case of preinstalled

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ setup(
     python_requires=">=3.8",
     dependency_links=["https://download.pytorch.org/whl/cu117"],
     install_requires=[
-        "torch==2.0.1+cu117",
-        "torchaudio==2.0.2+cu117",
-        "torchvision==0.15.2+cu117",
         "wandb==0.15.8",
         "transformers[torch] @ git+https://github.com/FoamoftheSea/transformers.git@multiformer",
         "evaluate==0.4.0",
         "shift_dev @ git+https://github.com/FoamoftheSea/shift-dev.git@bbox-loading",
         "torchmetrics==1.2.0"
     ],
+    extras_require={
+        "torch": [
+            "torch==2.0.1+cu117",
+            "torchaudio==2.0.2+cu117",
+            "torchvision==0.15.2+cu117",
+        ]
+    }
 )
